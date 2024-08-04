c2ba387df03aca351227642ac8d9fd139de06daa
Marco <marcoucr2@gmail.com>
2024-08-03 18:21:50 -0600

    Agregando archivo con los errores del punto6.py corregidos

---
+++ Tarea1/punto7.py
@@ -0,0 +1,5 @@
+# esta lína era intencionalemnte demasaido larga para poder estar en los
+# parámetros de flake8
+# eliminé muchas líneas en blanco
+# ahora incluí un espacio necesario en una definición
+lol = int
